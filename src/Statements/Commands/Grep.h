992f9fe4a945df334f095f1ae9f3ce32a79c8608
szymon <S.Szymborski@stud.elka.pw.edu.pl>
2018-06-13 13:04:47 +0200

    Grep implement

--- src/Statements/Commands/Grep.h
+++ src/Statements/Commands/Grep.h
@@ -1,22 +1,32 @@
-//
-// Created by dram on 06.06.18.
-//
-
 #ifndef SHELL_INTERPRETER_GREP_H
 #define SHELL_INTERPRETER_GREP_H
 
 #include "../Statement.h"
+#include <cstdio>
+#include <cstdlib>
 
 namespace Commands {
-    class Grep: public Statement {
+    class Grep : public Statement {
     public:
-        void execute() override{
-            std::cout << "grep: ";
-            for(auto & I : arguments){
-                std::cout << I << " ";
+        void execute() override {
+            std::string argument = "grep";
+            for (auto &I : arguments) {
+                argument += " ";
+                argument += I;
             }
-            std::cout << "\n";
-        };
+            FILE *cmd;
+            char result[1024];
+            cmd = popen(argument.c_str(), "r");
+            if (cmd == nullptr) {
+                perror("popen");
+                exit(EXIT_FAILURE);
+            }
+            while (fgets(result, sizeof(result), cmd)) {
+                printf("%s", result);
+            }
+            pclose(cmd);
+        }
     };
-}
+};
+
 #endif //SHELL_INTERPRETER_GREP_H
