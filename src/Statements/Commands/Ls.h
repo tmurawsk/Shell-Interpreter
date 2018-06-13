e88dfe1e3c581dc93cbca2965c13cf1d0ce00c5f
szymon <S.Szymborski@stud.elka.pw.edu.pl>
2018-06-13 13:40:43 +0200

    Ls implement

--- src/Statements/Commands/Ls.h
+++ src/Statements/Commands/Ls.h
@@ -1,7 +1,3 @@
-//
-// Created by dram on 06.06.18.
-//
-
 #ifndef SHELL_INTERPRETER_LS_H
 #define SHELL_INTERPRETER_LS_H
 
@@ -18,13 +14,43 @@ namespace Commands {
     class Ls : public Statement {
     public:
         void execute() override {
+            if (!arguments.empty() && arguments[0] == "-my") {
+                arguments.erase(arguments.begin());
+                myImplement();
+            } else {
+                std::string argument = "ls";
+                for (auto &I : arguments) {
+                    argument += " ";
+                    argument += I;
+                }
+                FILE *cmd;
+                char result[1024];
+                cmd = popen(argument.c_str(), "r");
+                if (cmd == nullptr) {
+                    perror("popen");
+                    exit(EXIT_FAILURE);
+                }
+                while (fgets(result, sizeof(result), cmd)) {
+                    printf("%s", result);
+                }
+                pclose(cmd);
+            }
+        }
 
+    private:
+        std::string getPermsForFile(struct stat &ret) {
+            int owner = (ret.st_mode & S_IRUSR) | (ret.st_mode & S_IWUSR) | (ret.st_mode & S_IXUSR); //owner
+            int group = (ret.st_mode & S_IRGRP) | (ret.st_mode & S_IWGRP) | (ret.st_mode & S_IXGRP); //group
+            int other = (ret.st_mode & S_IROTH) | (ret.st_mode & S_IWOTH) | (ret.st_mode & S_IXOTH); //other
+
+            return mapPermsToString(owner) + mapPermsToString(group) + mapPermsToString(other);
+        }
+
+        void myImplement() {
             bool l_flag = false, i_flag = false, a_flag = false;
-
             if (arguments.size() > 4) {
                 throw InvalidNumberOfParametersException();
             }
-
             for (int i = 0; i < arguments.size() && !arguments.empty(); i++) {
                 if (arguments[i][0] != '-') {
                     if (i < arguments.size() - 1)
@@ -46,89 +72,58 @@ namespace Commands {
                     }
                 }
             }
-
             DIR *dir;
             struct dirent *ent;
             std::stringstream files;
             int fileCounter = 0;
             int filesInOneLineCounter = 0;
             std::string path;
-
             path = arguments.empty() || arguments[arguments.size() - 1][0] == '-' ?
                    "." : arguments[arguments.size() - 1];
-
             std::string currDir = get_current_dir_name();
-
             if ((dir = opendir(path.c_str())) != NULL) {
                 while ((ent = readdir(dir)) != NULL) { //read files
                     if (l_flag) {
                         if (!a_flag && ent->d_name[0] == '.') { //ignore hidden
                             continue;
                         }
-
                         std::string s = currDir + '/' + path + '/' + std::string(ent->d_name);
-
                         struct stat ret;
                         if ((stat((currDir + '/' + path + '/' + std::string(ent->d_name)).c_str(),
                                   &ret)) == -1) {
 //                            throw InvalidArgumentsException();
                             continue;
                         }
-
                         if (i_flag) {
                             files << ent->d_ino << " "; //inode number
                         }
-
                         files << getFileTypeFromInt(ent->d_type) << " "; //file type
-
-
                         files << getPermsForFile(ret) << "\t"; //permissions
-
                         files << ret.st_nlink << "\t"; //number of hardlinks
-
                         files << getpwuid(ret.st_uid)->pw_name << "\t"; //owner name
-
                         files << getgrgid(ret.st_gid)->gr_name << "\t"; //group name
-
                         files << ret.st_size << "\t"; //file size in bytes
-
                         if (ret.st_size < 1000)
                             files << "\t";
-
                         files << ent->d_name << "\n"; //file name
                     } else {
                         if (!a_flag && ent->d_name[0] == '.') //ignore hidden
                             continue;
-
                         if (i_flag)
                             files << ent->d_ino << " "; //inode number
-
                         files << ent->d_name << "\t\t"; //file name
-
                         if (++filesInOneLineCounter == 4) {
                             filesInOneLineCounter = 0;
                             files << "\n";
                         }
                     }
-
                     fileCounter++;
                 }
             } else
                 throw NoSuchPathException();
-
             std::stringstream output;
             output << "total: " << fileCounter << "\n" << files.str();
-
             std::cout << output.str();
-        };
-
-    private:
-        std::string getPermsForFile(struct stat &ret) {
-            int owner = (ret.st_mode & S_IRUSR) | (ret.st_mode & S_IWUSR) | (ret.st_mode & S_IXUSR); //owner
-            int group = (ret.st_mode & S_IRGRP) | (ret.st_mode & S_IWGRP) | (ret.st_mode & S_IXGRP); //group
-            int other = (ret.st_mode & S_IROTH) | (ret.st_mode & S_IWOTH) | (ret.st_mode & S_IXOTH); //other
-
-            return mapPermsToString(owner) + mapPermsToString(group) + mapPermsToString(other);
         }
 
         std::string mapPermsToString(int perms) {
